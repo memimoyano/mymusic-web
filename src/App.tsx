79390ee9500fe87970e66a694652cc95f7ed60c0
memimoyano <moyanomemilia@gmail.com>
2024-06-02 12:22:03 -0300

    login - not finish

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import Login from './components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='flex bg-dark-night flex-col h-screen w-screen max-w-screen'>
+      <div className='flex bg-dark-night flex-col h-screen w-screen max-w-screen '>
         
         <div className='flex flex-col w-full h-full justify-center items-center'>
           <Login/>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import close_eye from '../images/close-eye.webp';
 import open_eye from '../images/open-eye.webp';
+import mymusic_icon from '../images/mymusic-periwinkle-icon.webp'
 
 
 export default function Login(){
@@ -9,51 +10,63 @@ export default function Login(){
     const [showPassword, setShowPassword] = useState(false);
 
     return(
-        <form className="flex flex-col w-1/2 bg-night rounded p-2 justify-between
-            text-ivory items-center gap-5
-        ">
+        <div className="flex flex-col w-1/2 bg-night rounded p-6 justify-between
+        text-ivory items-center gap-5">
 
-            <h1 className="font-bold text-5xl">
-                Log in - My Music
-            </h1>
-
-            <section className="flex flex-col justify-between gap-2">
-                <label htmlFor="email" className="text-3xl font-medium">
-                    Email
-                </label>
-                <input id="email" type="text"/>
+            <section className="flex flex-col items-center justify-between gap-3 
+            border-black-olive border-b pb-3">
+                <img src={mymusic_icon} className="w-1/6"/>
+                <h1 className="font-medium text-4xl text-light-periwinkle tracking-wide">
+                    Log in  My Music
+                </h1>
             </section>
 
-            <section className="flex flex-col justify-between gap-2 "> 
-                <label htmlFor="password">
-                    Password
-                </label>
-               
-                <div className="flex flex-row items-center gap-1">
-                    <input 
-                        id="password" 
-                        type={showPassword ? "text" : "password"}
-                        value={password}
-                        onChange={(e) =>
-                            setPassword(e.target.value)
-                        }
+            <form className="flex flex-col justify-between gap-4 w-4/5">
+
+                <section className="flex flex-col w-full justify-between gap-2">
+                    <label htmlFor="email" className="text-xl font-medium">
+                        Email
+                    </label>
+                    <input id="email" type="text" placeholder="Email"
+                    className="flex flex-row h-10 box-border bg-night rounded-sm border border-ivory 
+                    items-center px-1 focus:outline-periwinkle" 
                     />
+                </section>
 
-                    {showPassword &&
-                        <button>
-                            <img src={open_eye} className="h-5 w-5"/>
-                        </button>
-                    }
-                    {!showPassword &&
-                        <button>
-                            <img src={close_eye} className="h-5 w-5"/>
-                        </button>
-                    }
-                </div>
+                <section className="flex flex-col w-full justify-between gap-2 "> 
+                    <label htmlFor="password" className="text-xl font-medium">
+                        Password
+                    </label>
+                
+                    <div className="flex flex-row justify-between h-10
+                    items-center rounded-sm border border-ivory">
+                        <input 
+                            id="password" 
+                            placeholder="Password"
+                            type={showPassword ? "text" : "password"}
+                            value={password}
+                            onChange={(e) =>
+                                setPassword(e.target.value)
+                            }
+                            className="h-9 bg-night focus:outline-none px-1 w-full"
+                        />
 
-            </section>
+                        {showPassword &&
+                            <button>
+                                <img src={open_eye} className="h-5 w-5"/>
+                            </button>
+                        }
+                        {!showPassword &&
+                            <button>
+                                <img src={close_eye} className="h-5 w-5"/>
+                            </button>
+                        }
+                    </div>
 
-            <input type="submit" value={"Log in"}/>
-        </form>
+                </section>
+
+                <input type="submit" value={"Log in"} className="btn-primary"/>
+            </form>
+        </div>
     )
 }
